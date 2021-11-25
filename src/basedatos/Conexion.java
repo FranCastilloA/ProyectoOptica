4effa6a1ad5e09884761e8c89a71da55438e4de8
abarr <abarr@LAPTOP-308M00BN>
2021-11-25 20:03:51 -0300

    Corrección nombre del packages y de la base de datos de la coneccion, base de datos optica.

--- src/basedatos/Conexion.java
+++ src/basedatos/Conexion.java
@@ -1,5 +1,5 @@
 
-package bd;
+package basedatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,7 +13,7 @@ public class Conexion {
         Connection connection = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/libreria","root","");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/optica","root","");
             System.out.println("Conexion exitosa");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
