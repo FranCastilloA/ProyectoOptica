719a88659cc105ebaeb9f3795ae6abfaa936552e
abarr <abarr@LAPTOP-308M00BN>
2021-12-01 14:37:42 -0300

    Se crearon vistas de menus, Metodo Producto y Controlador Producto

--- src/Controlador/ProductoControlador.java
+++ src/Controlador/ProductoControlador.java
@@ -1,14 +1,196 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controlador;
+import BaseDeDatos.Conexion;
+import Modelo.Producto;
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Neotemplar-R480
+ * @author abarr
+ * @date 30/11/21
  */
 public class ProductoControlador {
+    //Crear Producto  
+   public boolean agregarProducto(Producto producto) throws SQLException{
+       
+       try {
+           Conexion con = new Conexion();
+           java.sql.Connection cnx = con.obtenerConexion();
+           String query ="INSERT INTO producto(nombre_producto, tipo_producto, descripcion, precio, cantidad, disponible)VALUES(?,?,?,?,?,?)";
+           PreparedStatement stmt = cnx.prepareStatement(query);
+           stmt.setString(1,producto.getNombre_producto());
+           stmt.setString(2,producto.getTipo_producto());
+           stmt.setString(3,producto.getDescripcion());
+           stmt.setInt(4, producto.getPrecio());
+           stmt.setInt(5, producto.getCantidad());
+           stmt.setBoolean(6,producto.isDisponible());
+           
+           stmt.executeUpdate();
+           stmt.close();
+           cnx.close();
+           
+           return true; 
+           
+       } catch (SQLException e) {
+           System.out.println("Error SQL al agregar Producto"+ e.getMessage());
+           return false;
+       }
+        
+   }
+    //Modificar Producto
+    public boolean modificarProducto(Producto producto) throws SQLException{
+       
+       try {
+           Conexion con = new Conexion();
+           java.sql.Connection cnx = con.obtenerConexion();
+           String query ="UPDATE produco set nombre_producto=?, tipo_producto=?, descripcion=?, precio=?, cantidad=?, disponible=?)WHERE id_producto=?";
+           PreparedStatement stmt = cnx.prepareStatement(query);
+           stmt.setString(1,producto.getNombre_producto());
+           stmt.setString(2,producto.getTipo_producto());
+           stmt.setString(3,producto.getDescripcion());
+           stmt.setInt(4, producto.getPrecio());
+           stmt.setInt(5, producto.getCantidad());
+           stmt.setBoolean(6,producto.isDisponible());
+           stmt.setInt(7,producto.getId_producto());
+           
+           stmt.executeUpdate();
+           stmt.close();
+           cnx.close();
+           
+           return true; 
+           
+       } catch (SQLException e) {
+           System.out.println("Error SQL al Modificar Producto"+ e.getMessage());
+           return false;
+       }
+        
+   }    
+       //Eliminar Producto
+    public boolean eliminarProducto(String nombre_producto) throws SQLException{
+       
+       try {
+           Conexion con = new Conexion();
+           java.sql.Connection cnx = con.obtenerConexion();
+           String query ="DELETE FROM produco nombre_producto=?";
+           PreparedStatement stmt = cnx.prepareStatement(query);
+           stmt.setString(1,nombre_producto);
+           
+           
+           stmt.executeUpdate();
+           stmt.close();
+           cnx.close();
+           
+           return true; 
+           
+       } catch (SQLException e) {
+           System.out.println("Error SQL al Eliminar Producto"+ e.getMessage());
+           return false;
+       }
+        
+   }
+   
+    //Buscar Producto por el nombre 
+    public Producto buscarProducto (String nombre_producto){
+        Producto producto = new Producto();
+        
+        try {
+           Conexion con = new Conexion();
+           java.sql.Connection cnx = con.obtenerConexion();
+           String query ="SELECT id_producto, nombre_producto, tipo_producto, descripcion, precio, cantidad, disponible FROM producto WHERE nombre_producto=?";
+           PreparedStatement stmt = cnx.prepareStatement(query);
+           stmt.setString(1,nombre_producto);
+           
+           ResultSet rs = stmt.executeQuery();
+           while(rs.next()){
+               producto.setId_producto(rs.getInt("id_producto"));
+               producto.setNombre_producto(rs.getString("nombre_producto"));
+               producto.setTipo_producto(rs.getString("tipo_producto"));
+               producto.setPrecio(rs.getInt("precio"));
+               producto.setCantidad(rs.getInt("cantidad"));
+               producto.setDisponible(rs.getBoolean("disponible"));
+               
+           }
+           rs.close();
+           stmt.close();
+           cnx.close();
+           
+       } catch (SQLException e) {
+           System.out.println("Error SQL al Buscar Producto"+ e.getMessage());
+           
+       }
+        return producto;
+    }
+  
+        //Buscar Producto por Tipo de producto
+    public Producto buscarTipoProducto (String tipo_producto){
+        Producto producto = new Producto();
+        
+        try {
+           Conexion con = new Conexion();
+           java.sql.Connection cnx = con.obtenerConexion();
+           String query ="SELECT id_producto, nombre_producto, tipo_producto, descripcion, precio, cantidad, disponible FROM producto WHERE tipo_producto=?";
+           PreparedStatement stmt = cnx.prepareStatement(query);
+           stmt.setString(1,tipo_producto);
+           
+           ResultSet rs = stmt.executeQuery();
+           while(rs.next()){
+               producto.setId_producto(rs.getInt("id_producto"));
+               producto.setNombre_producto(rs.getString("nombre_producto"));
+               producto.setTipo_producto(rs.getString("tipo_producto"));
+               producto.setPrecio(rs.getInt("precio"));
+               producto.setCantidad(rs.getInt("cantidad"));
+               producto.setDisponible(rs.getBoolean("disponible"));
+               
+           }
+           rs.close();
+           stmt.close();
+           cnx.close();
+           
+       } catch (SQLException e) {
+           System.out.println("Error SQL al Buscar Producto"+ e.getMessage());
+           
+       }
+        return producto;
+    }
+      //Listar productos
+    public List<Producto> buscarTodos(){
+        List<Producto> lista =new ArrayList<>();
+        
+        try {
+           Conexion con = new Conexion();
+           java.sql.Connection cnx = con.obtenerConexion();
+           String query ="SELECT id_producto, nombre_producto, tipo_producto, descripcion, precio, cantidad, disponible FROM producto ORDER BY tipo_producto";
+           PreparedStatement stmt = cnx.prepareStatement(query);
+           
+           ResultSet rs = stmt.executeQuery();
+           
+           while(rs.next()){
+               Producto producto =new Producto();
+               producto.setId_producto(rs.getInt("id_producto"));
+               producto.setNombre_producto(rs.getString("nombre_producto"));
+               producto.setTipo_producto(rs.getString("tipo_producto"));
+               producto.setPrecio(rs.getInt("precio"));
+               producto.setCantidad(rs.getInt("cantidad"));
+               producto.setDisponible(rs.getBoolean("disponible"));
+               
+               lista.add(producto);
+           }
+           rs.close();
+           stmt.close();
+           cnx.close();
+           
+       } catch (SQLException e) {
+           System.out.println("Error SQL al Listar Producto"+ e.getMessage());
+           
+       }
+        return lista;
+            
+    }
+}    
     
-}
+  
--- src/Modelo/Producto.java
+++ src/Modelo/Producto.java
@@ -7,8 +7,91 @@ package Modelo;
 
 /**
  *
- * @author Neotemplar-R480
+ * @author abarr
  */
 public class Producto {
+    
+    private int id_producto;
+    private String nombre_producto;
+    private String tipo_producto;
+    private String descripcion;
+    private int precio;
+    private int cantidad;
+    private boolean disponible;
+
+    public Producto() {
+    }
+
+    public Producto(int id_producto, String nombre_producto, String tipo_producto, String descripcion, int precio, int cantidad, boolean disponible) {
+        this.id_producto = id_producto;
+        this.nombre_producto = nombre_producto;
+        this.tipo_producto = tipo_producto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.disponible = disponible;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getTipo_producto() {
+        return tipo_producto;
+    }
+
+    public void setTipo_producto(String tipo_producto) {
+        this.tipo_producto = tipo_producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+    
+   
+
+    
     
 }
